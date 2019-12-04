1401d09eb6554682b731da1e47728ca96bcca56d
Pavel Shakhlovich <Altmersc@gmail.com>
2019-12-04 23:28:13 +0300

    tests

--- src/test/java/by/epam/pavelshakhlovich/informationhandling/action/TextActionTest.java
+++ src/test/java/by/epam/pavelshakhlovich/informationhandling/action/TextActionTest.java
@@ -10,22 +10,40 @@ import org.testng.annotations.Test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class TextActionTest {
     private static DataReader reader = new DataReader();
     private static PrimaryParser primaryParser = new PrimaryParser();
-    private static Path path1 = Paths.get("src/test/test_data/testData1.txt");
+    private static Path path1 = Paths.get("src/test/test_data/testData3.txt");
     private static Path path2 = Paths.get("src/test/test_data/testData2.txt");
 
 
     @Test
     public void testSortParagraphsBySentenceCount() {
+        String text = reader.readTextFromFile(path1);
+        TextComponent textComponent = new CompositeTextComponent(TextPartType.TEXT, text);
+        primaryParser.handleText(textComponent);
+        TextAction.sortParagraphsBySentenceCount(textComponent);
+        String actualString = textComponent.getStoredString().trim();
+        String expectedText = "Two words. One.\n\t" +
+                "One. Three words here. Two words.\r\n\t" +
+                "Sixteen four a three two it. One. Three words here. Two words.";
+        assertEquals(actualString, expectedText);
+
     }
 
     @Test
     public void testSortSentencesByWordsCount() {
-
+        String text = reader.readTextFromFile(path1);
+        TextComponent textComponent = new CompositeTextComponent(TextPartType.TEXT, text);
+        primaryParser.handleText(textComponent);
+        TextAction.sortSentencesByWordsCount(textComponent);
+        String actualString = textComponent.getStoredString().trim();
+        String expectedText = "One. Two words. Three words here. Sixteen four a three two it. \n\t" +
+                "One. Two words. Three words here. \n\t" +
+                "One. Two words.";
+                assertEquals(actualString, expectedText);
     }
 
     @Test
